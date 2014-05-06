1fbb6b833b6df4faf87984815d4585f29458f97e
dbolotov <dbolotov@live.com>
2014-05-06 15:10:34 -0700

    added code to plot

--- plot4.R
+++ plot4.R
@@ -21,13 +21,26 @@ data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:
 
 
 #create and save plot
-png(filename="plot3.png",width=480,height=480)
-plot(data$DateTime,data$Sub_metering_1,type="l",col="black",
-     xlab="", ylab="Energy sub metering")
-lines(data$DateTime,data$Sub_metering_2,type="l",col="red")
-lines(data$DateTime,data$Sub_metering_3,type="l",col="blue")
+png(filename="plot4.png",width=480,height=480)
 
-legend("topright", lty=c(1,1),col = c("black","red","blue"), 
-       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+par(mfrow=c(2,2))
 
+with(data,{
+  plot(data$DateTime,data$Global_active_power,type="l",
+       xlab="", ylab="Global Active Power")
+  plot(data$DateTime,data$Voltage,type="l",
+       xlab="datetime", ylab="Voltage")
+  plot(data$DateTime,data$Sub_metering_1,type="l",col="black",
+       xlab="", ylab="Energy sub metering")
+  lines(data$DateTime,data$Sub_metering_2,type="l",col="red")
+  lines(data$DateTime,data$Sub_metering_3,type="l",col="blue")
+  legend("topright", lty=c(1,1),col = c("black","red","blue"),bty="n",
+         legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  plot(data$DateTime,data$Global_reactive_power,type="l",
+       xlab="datetime", ylab="Global_reactive_power")
+})
 dev.off()
+
+
+
+
