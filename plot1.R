fc24894406e4eae28e2d18006acb62ec261593c3
dbolotov <dbolotov@live.com>
2014-05-05 23:01:38 -0700

    init

---
+++ plot1.R
@@ -0,0 +1,9 @@
+# Coursera - Exploratory Data Analysis
+#
+# Course Project 1
+#
+# Plot 1
+
+setwd("E:/documents/progr/ExData_Plotting1")
+
+data <- read.table("household_power_consumption.txt",comment.char="")
