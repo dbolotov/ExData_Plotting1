c773bdd5b4c58fe5d3f367449390d19326e1571d
dbolotov <dbolotov@live.com>
2014-05-06 12:41:18 -0700

    added code to read, plot and save as png

--- plot1.R
+++ plot1.R
@@ -1,9 +1,32 @@
 # Coursera - Exploratory Data Analysis
-#
 # Course Project 1
-#
-# Plot 1
 
 setwd("E:/documents/progr/ExData_Plotting1")
 
-data <- read.table("household_power_consumption.txt",comment.char="")
+#load data
+initial <- read.table("household_power_consumption.txt",
+                      header=TRUE,sep=";",quote="",comment.char="",
+                      nrows=100)
+
+classes <- sapply(initial,class)
+
+ptm <- proc.time()
+data <- read.table("household_power_consumption.txt",
+                   header=FALSE,sep=";",quote="",na.strings="?",comment.char="",
+                   colClasses = classes,skip = 66637,nrows=2880)
+proc.time() - ptm
+
+#preprocess
+names(data) <- names(initial)
+
+data$DateTime <- paste(data$Date,data$Time)
+data$DateTime <- strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+#create and save plot
+png(filename="plot1.png",width=480,height=480)
+hist(data$Global_active_power,
+      col="red",main="Global Active Power",
+      xlab="Global Active Power (kilowatts)",ylab="Frequency")
+
+dev.off()
+
